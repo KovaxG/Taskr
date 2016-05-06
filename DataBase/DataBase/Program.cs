47d69377dd54e57d06a919d760e28ffb73d3f73c
KovaxG <georgesmth202@gmail.com>
2016-05-06 18:47:10 +0300

    Massive Overhaul on the DataBaseHandler, 70% Done

--- DataBase/DataBase/Program.cs
+++ DataBase/DataBase/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using DataBase;
+using System.Linq;
 
 namespace DataBase
 {
@@ -12,10 +13,6 @@ namespace DataBase
 
 		public static void Main (string[] args)
 		{
-			//UserData user = new UserData (1);
-			//MainClass tester = new MainClass();
-			//tester.Login();
-
 			MainClass main = new MainClass ();
 			main.run ();
 
@@ -24,42 +21,55 @@ namespace DataBase
 
 		public void run () {
 
-			if (!db.Test ()) return;
+			// Check if can connect to database-------------------------------------------------
+			Console.WriteLine ("Attempting to connect to database...");
+			if (!db.Test ()) {
+				Console.WriteLine ("Could not connect to database!");
+				return;
+			} else
+				Console.WriteLine ("Connection to database succesfull.");
+			// End of Check----------------------------------------------------------------------
 
-			try {
-				Console.WriteLine ("This is a test of Taskr.\n\n");
-				Console.WriteLine ("Login\n\n");
-				Console.Write ("UserName: ");
-				string username = "mircea";//Console.ReadLine ();
-				Console.Write ("Password: ");
-				string password = "passwordmircea1";//
-				db.Login (username, password);
+			// Login-----------------------------------------------------------------------------
+			Console.WriteLine ("Login\n\n");
+			Console.Write ("UserName: ");
+			string username = "Pizdosu";//Console.ReadLine ();
+			Console.Write ("Password: ");
+			string password = "passwordmircea1";//
+			if (db.Login (username, password)) {
 				Console.WriteLine ("\n\nSuccessful login.");
 				printUserDetails (db.User);
-
-				List<ProjectData> projectList = db.GetActiveProjectsList ();
-				List<ProjectSuggestionData> projectSuggestionList = db.GetProjectSuggestionsList ();
-
-				Console.WriteLine ("\n========================================");
-				Console.WriteLine ("Printing Projects List: \n");
-				foreach (ProjectData p in projectList) {
-					Console.WriteLine (p.Title);
-				}
-				foreach (ProjectSuggestionData ps in projectSuggestionList) {
-					Console.WriteLine (ps.Title);
-				}
-				Console.WriteLine ("\n========================================");
 			}
-			catch(Exception e) 
-			{
-				Console.WriteLine (e.ToString ());
+			Console.WriteLine ("Login not succesfull.");
+			// End of Login-----------------------------------------------------------------------
+
+			viewAvailibleProjects ();
+
+		}
+
+		public void viewAvailibleProjects() {
+			List<ProjectData> projectList = db.GetActiveProjectsList ();
+			List<ProjectSuggestionData> projectSuggestionList = db.GetProjectSuggestionsList ();
+
+			Console.WriteLine ("\n========================================");
+			Console.WriteLine ("Printing Projects List: \n");
+
+			foreach (ProjectData p in projectList) {
+				Console.WriteLine (p.Title);
 			}
+
+			Console.WriteLine ("\n\nPrinting ProjectSuggestions List: \n");
+			foreach (ProjectSuggestionData ps in projectSuggestionList) {
+				Console.WriteLine (ps.Title);
+			}
+			Console.WriteLine ("\n========================================");
 
 		}
 			
 
 		// User details can be used to fill in any data you want from the form
 		public void printUserDetails (UserData user) {
+			if (user == null) return;
 			Console.WriteLine ("\n========================================");
 			Console.WriteLine ("Printing user details...\n");
 			Console.WriteLine ("FirstName: " + user.FirstName);
@@ -78,7 +88,7 @@ namespace DataBase
 			Console.WriteLine ("DateLeft: " + user.DateLeft.ToString ());
 			Console.WriteLine ("ReasonForLeaving: " + user.ReasonForLeaving);
 			Console.WriteLine ("RejoinDesirability: " + user.RejoinDesirability);
-			Console.WriteLine ("Observations: " + user.Observations);
+			Console.WriteLine ("Observations: " + user.Notes);
 			Console.WriteLine ("\n========================================");
 		}
 	}
